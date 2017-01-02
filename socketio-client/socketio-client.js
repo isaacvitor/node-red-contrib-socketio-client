0f89db4de8e33cda61cf8ba95a02b4cd46a71081
Isaac Vitor <isaacvitor@gmail.com>
2017-01-02 09:18:08 -0300

    add node

---
+++ socketio-client/socketio-client.js
@@ -0,0 +1,68 @@
+module.exports = function(RED) {
+  'use strict';
+  var io = require('socket.io');
+
+  /* sckt config */
+  function SocketIOConfig(config) {
+    RED.nodes.createNode(this, config);
+    this.host = config.host;
+    this.port = config.port;
+  }
+  RED.nodes.registerType('socketio-config', SocketIOConfig);
+
+  /* sckt connector*/
+  function SocketIOConnector(config){
+    RED.nodes.createNode(this, config);
+    this.server = RED.nodes.getNode(config.server);
+    this.name = config.name;
+    this.event = config.event;
+    this.sto = null;
+    var node = this;
+
+    this.client = connect(this.server);
+
+    node.status({fill:"green",shape:"dot",text:"connected"});
+
+    node.on('input', function(msg) {
+      msg.payload = this.client;
+      node.send(msg);
+    });
+
+    node.on('close', function(done) {
+      node.status({});
+      disconnect(node.server);
+      done();
+    });
+
+    node.client.on('error', function(err) {
+      if (err) {
+        clearInterval(node.sto);
+        node.error(err);
+      }
+    });  
+  }
+  RED.nodes.registerType('socketio-connector', SocketIOConnector);
+
+
+  function connect(config) {
+    var uri = (config.port != '') ? (config.host + ':' + config.port) : config.host;
+    var server = (config.namespace) ? uri + "/" + config.namespace : uri;
+    var sckt = io( server );
+
+    sckt.on('connect_error', function(err) {
+      console.log('[socket.io]', err);
+    });
+    return sckt;
+  }
+
+  function disconnect(config) {
+      /*var idx = config.pass + '@' + config.host + ':' + config.port + '/' + config.dbase;
+      if (usingConn[idx] !== undefined) {
+          usingConn[idx]--;
+
+      }
+      if (usingConn[idx] <= 0) {
+          connection[idx].end();
+      }*/
+  }
+}
